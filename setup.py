615434053520a9ba3bc57c0c12ee9c832a89208b
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-04-20 01:07:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import find_packages, setup
+
+setup(
+    name="noise_reduction_autoencoder",
+    version="0.0.0",
+    author="GKPrarthana",
+    author_email="39-dba-0025@kdu.ac.lk",
+    packages=find_packages(),
+    install_requires=[],
+)
